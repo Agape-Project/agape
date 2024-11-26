22284f8e5d5956636bad226e99568718c0cb4ea8
EphriamHab <ephremhabtamu2015@gmail.com>
2024-11-26 22:43:50 +0300

    wrap with providerScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:agape/common/splash.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,8 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: SplashScreen(),
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
     );
   }
 }
