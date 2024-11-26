ca060e1b33f3d4f53420fd0439622eeb9e84ff17
EphriamHab <ephremhabtamu2015@gmail.com>
2024-11-26 22:45:49 +0300

    text field

---
+++ lib/widgets/text_field.dart
@@ -0,0 +1,78 @@
+import 'package:agape/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+class TextFieldInput extends StatefulWidget {
+  final TextEditingController textEditingController;
+  final bool isPass;
+  final String hintText;
+  final IconData? icon;
+  final TextInputType textInputType;
+
+  const TextFieldInput({
+    Key? key,
+    required this.textEditingController,
+    this.isPass = false,
+    required this.hintText,
+    this.icon,
+    required this.textInputType,
+  }) : super(key: key);
+
+  @override
+  _TextFieldInputState createState() => _TextFieldInputState();
+}
+
+class _TextFieldInputState extends State<TextFieldInput> {
+  late bool isObscure;
+
+  @override
+  void initState() {
+    super.initState();
+    isObscure = widget.isPass; 
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      child: TextField(
+        style: const TextStyle(fontSize: 20),
+        controller: widget.textEditingController,
+        decoration: InputDecoration(
+          prefixIcon: Icon(widget.icon, color: Colors.black54),
+          suffixIcon: widget.isPass
+              ? IconButton(
+                  onPressed: () {
+                    setState(() {
+                      isObscure = !isObscure; 
+                    });
+                  },
+                  icon: Icon(
+                    isObscure ? Icons.visibility : Icons.visibility_off,
+                    color: Colors.black54,
+                  ),
+                )
+              : null,
+          hintText: widget.hintText,
+          hintStyle: const TextStyle(color: Colors.black45, fontSize: 18),
+          enabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.grey, width: 0.5),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          border: InputBorder.none,
+          focusedBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color: primaryColor, width: 2),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          filled: true,
+          fillColor: const Color(0xFFedf0f8),
+          contentPadding: const EdgeInsets.symmetric(
+            vertical: 15,
+            horizontal: 20,
+          ),
+        ),
+        keyboardType: widget.textInputType,
+        obscureText: isObscure,
+      ),
+    );
+  }
+}
