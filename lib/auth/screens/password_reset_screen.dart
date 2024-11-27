0a50f81451870864767a35ee97017f8d679b2547
EphriamHab <ephremhabtamu2015@gmail.com>
2024-11-27 19:50:18 +0300

    password reset screen

---
+++ lib/auth/screens/password_reset_screen.dart
@@ -0,0 +1,160 @@
+import 'package:agape/auth/controllers/auth_controller.dart';
+import 'package:agape/auth/screens/login_screen.dart';
+import 'package:agape/widgets/snackbar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ResetPasswordPage extends ConsumerStatefulWidget {
+  @override
+  _ResetPasswordPageState createState() => _ResetPasswordPageState();
+}
+
+class _ResetPasswordPageState extends ConsumerState<ResetPasswordPage> {
+  final _formKey = GlobalKey<FormState>();
+  bool _newPasswordVisible = false;
+  bool _confirmPasswordVisible = false;
+
+  String? _newPassword;
+  String? _confirmPassword;
+
+  final TextEditingController _newPasswordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+
+  String? _confirmPasswordValidator(String? value) {
+    if (value!.isEmpty) {
+      return "Please confirm new password";
+    }
+    if (value != _newPasswordController.text) {
+      return "Passwords do not match";
+    }
+    return null;
+  }
+
+  void _submitForm() async {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      final newPassword = _newPasswordController.text.trim();
+      final confirmPassword = _confirmPasswordController.text.trim();
+
+      final authController = ref.read(authControllerProvider);
+
+      await authController.setNewPassword(newPassword, confirmPassword);
+
+      Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (context) => const LoginScreen()));
+    } else {
+      showSnackBar(context, "Please enter your new password");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Password Reset"),
+        centerTitle: true,
+      ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          final isLargeScreen = constraints.maxWidth > 600;
+          final formWidth = isLargeScreen ? 400.0 : constraints.maxWidth;
+
+          return Center(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(16.0),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: formWidth),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      // New Password
+                      TextFormField(
+                        controller: _newPasswordController,
+                        obscureText: !_newPasswordVisible,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          labelText: "New Password",
+                          prefixIcon: Icon(Icons.lock),
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              _newPasswordVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _newPasswordVisible = !_newPasswordVisible;
+                              });
+                            },
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Please enter new password";
+                          }
+                          if (value.length < 6) {
+                            return "Password must be at least 6 characters long";
+                          }
+                          return null;
+                        },
+                        onSaved: (value) => _newPassword = value,
+                      ),
+
+                      SizedBox(height: 16.0),
+
+                      // Confirm New Password
+                      TextFormField(
+                        controller: _confirmPasswordController,
+                        obscureText: !_confirmPasswordVisible,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          labelText: "Confirm New Password",
+                          prefixIcon: Icon(Icons.lock),
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              _confirmPasswordVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _confirmPasswordVisible =
+                                    !_confirmPasswordVisible;
+                              });
+                            },
+                          ),
+                        ),
+                        validator: _confirmPasswordValidator,
+                        onSaved: (value) => _confirmPassword = value,
+                      ),
+
+                      SizedBox(height: 32.0),
+
+                      // Submit Button
+                      ElevatedButton(
+                        onPressed: _submitForm,
+                        child: Text("Submit"),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(255, 9, 19, 58),
+                          foregroundColor: Colors.white,
+                          minimumSize: const Size(double.infinity, 50),
+                          textStyle: const TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
