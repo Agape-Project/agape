d8bbcf1bdb786348effac38ba4f176bb36832359
EphriamHab <ephremhabtamu2015@gmail.com>
2024-11-27 19:49:52 +0300

    forgot password

--- lib/auth/screens/forgot_password.dart
+++ lib/auth/screens/forgot_password.dart
@@ -1,9 +1,13 @@
+import 'package:agape/auth/controllers/auth_controller.dart';
 import 'package:agape/auth/screens/login_screen.dart';
+import 'package:agape/auth/screens/otp_screen.dart';
 import 'package:agape/widgets/button.dart';
+import 'package:agape/widgets/snackbar.dart';
 import 'package:agape/widgets/text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ForgotPasswordScreen extends StatefulWidget {
+class ForgotPasswordScreen extends ConsumerStatefulWidget {
   static const routeName = '/forgot-password';
   const ForgotPasswordScreen({Key? key}) : super(key: key);
 
@@ -11,7 +15,7 @@ class ForgotPasswordScreen extends StatefulWidget {
   _ForgotPasswordScreenState createState() => _ForgotPasswordScreenState();
 }
 
-class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+class _ForgotPasswordScreenState extends ConsumerState<ForgotPasswordScreen> {
   final _emailController = TextEditingController();
 
   @override
@@ -22,75 +26,68 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
 
   void sendPasswordResetLink() {
     final email = _emailController.text.trim();
-
-    // if(email.isNotEmpty){
-    //   ref.read(authControllerProvider).sendPasswordResetEmail(
-    //     context: context,
-    //     email: email,
-    //   );
-    // } else {
-    //   showSnackBar(context, "Please enter your email");
-    // }
- 
+    if (email.isNotEmpty) {
+      ref.read(authControllerProvider).sendPasswordResetEmail(
+            context: context,
+            email: email,
+          );
+          
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => OtpVerificationScreen(email: email),
+        ),
+      );
+    } else {
+      showSnackBar(context, "Please enter your email");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
+    double maxWidth = 400.0;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Forgot Password"),
+        centerTitle: true,
         backgroundColor: Theme.of(context).colorScheme.primary,
       ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'images/amico.png',
-                height: 200,
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                "Enter your Email address below and we will send you code to reset it",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16),
-              ),
-              const SizedBox(height: 20),
-              TextFieldInput(
-                textEditingController: _emailController,
-                hintText: "Email",
-                icon: Icons.email,
-                textInputType: TextInputType.emailAddress,
-              ),
-              const SizedBox(height: 20),
-              MyButtons (
-                onTap: sendPasswordResetLink,
-                text: "Send",
-                
-              ),
-              ElevatedButton(
-                      onPressed: () {
-                  // Navigate to the next screen or complete onboarding
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const LoginScreen(), 
+        child: Center(
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 16.0),
+            child: ConstrainedBox(
+              constraints: BoxConstraints(maxWidth: maxWidth),
+              child: Align(
+                alignment: Alignment.center,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text(
+                      "Enter your Email address below and we will send you a code to reset it",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 20),
                     ),
-                  );
-                },
-                      style: ElevatedButton.styleFrom(
-                            backgroundColor: Color.fromRGBO(255, 255, 255, 1), // Blue-black background color
-                            foregroundColor: Color.fromRGBO(9, 19, 58, 1), // White text color
-                            minimumSize: Size(150, 50), // Ensures consistent size
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8), // Slightly curved edges
-                            ),
-                          ),
-                      child: const Text("Log in"),
+                    const SizedBox(height: 20),
+                    TextFieldInput(
+                      textEditingController: _emailController,
+                      hintText: "Email",
+                      icon: Icons.email,
+                      textInputType: TextInputType.emailAddress,
                     ),
-            ],
+                    const SizedBox(height: 20),
+                    MyButtons(
+                      onTap: sendPasswordResetLink,
+                      text: "Send",
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ),
       ),
