ac580029aa3d570f08dc979e8f45fd3f535f32ad
EphriamHab <ephremhabtamu2015@gmail.com>
2024-11-27 19:49:36 +0300

    auth repository

--- lib/auth/repository/auth_repository.dart
+++ lib/auth/repository/auth_repository.dart
@@ -49,4 +49,73 @@ class AuthRepository {
           jsonDecode(response.body)['detail'] ?? 'invalid credentials');
     }
   }
+
+  Future<String> sendPasswordResetEmail(Map<String, dynamic> userData) async {
+    final url = Uri.parse('$baseUrl/api/auth/reset-password/');
+    final response = await http.post(
+      url,
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode(userData),
+    );
+
+    if (response.statusCode == 200) {
+      return "Password reset link sent to your email";
+    } else {
+      throw Exception(jsonDecode(response.body)['detail'] ??
+          'Error sending password reset link');
+    }
+  }
+
+  Future<String> verifyOTP(Map<String, dynamic> userData) async {
+    final url = Uri.parse('$baseUrl/api/auth/verify-otp/');
+    try {
+      final response = await http.post(
+        url,
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode(userData),
+      );
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        if (data['status'] == 'success') {
+          return data['message'];
+        } else {
+          return "OTP verification failed";
+        }
+      } else if (response.statusCode == 400) {
+        final data = jsonDecode(response.body);
+        return data['detail'] ?? "Bad request, please try again";
+      } else if (response.statusCode == 404) {
+        return "OTP verification endpoint not found";
+      } else {
+        final data = jsonDecode(response.body);
+        return data['detail'] ?? "Error verifying OTP";
+      }
+    } catch (e) {
+      return "Failed to verify OTP. Please check your internet connection.";
+    }
+  }
+
+Future<String> setNewPassword(Map<String, dynamic> userData) async { 
+  final url = Uri.parse('$baseUrl/api/auth/set-new-password/');
+  try {
+    final response = await http.post(
+      url,
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode(userData), 
+    );
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return data['message'] ?? "Password successfully updated";
+    } else {
+      return "Failed to set new password: ${response.reasonPhrase}"; 
+    }
+  } catch (e) {
+    return "An error occurred: $e"; 
+  }
+}
+
+
+
+
+
 }
