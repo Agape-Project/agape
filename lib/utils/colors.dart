0aa156651e90e5aa8587b749b24ce5bc76018822
EphriamHab <ephremhabtamu2015@gmail.com>
2024-11-28 21:23:27 +0300

    colors

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -2,8 +2,12 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 const primaryColor = Color(0xFF09133A);
-const secondaryColor = Color(0xFFE5E5E5);
+const secondaryColor = Color(0xFFF8F4F4);
 const accentColor = Color(0xFFE5E5E5);
 const errorColor = Color(0xFFD32F2F);
 const successColor = Color(0xFF4CAF50);
 const warningColor = Color(0xFFFFA000);
+const pieOne = Color(0xFFF105AE);
+const pieTwo = Color(0xFF0050FF);
+const pieThree = Color(0xFF05F138);
+const pieFour = Color(0xFF547FDD);
