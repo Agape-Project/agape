1f90b43e199f6ea7ddc5912e5446a46f12fe0bda
EphriamHab <ephremhabtamu2015@gmail.com>
2024-12-05 18:52:41 +0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'package:agape/admin/screens/bottom_nav.dart';
 import 'package:agape/admin/screens/form_screen.dart';
 import 'package:agape/auth/screens/forgot_password.dart';
 import 'package:agape/auth/screens/login_screen.dart';
-import 'package:agape/common/record_list.dart';
-import 'package:agape/common/register_record.dart';
 import 'package:agape/common/splash.dart';
 import 'package:agape/widgets/loading_animation_widget.dart';
 import 'package:flutter/material.dart';
