bc41d38ec4618f2b262df6b93c4c7c292f8bc7c6
EphriamHab <ephremhabtamu2015@gmail.com>
2024-12-05 18:55:47 +0300

    profile screen

--- lib/admin/screens/profile_screen.dart
+++ lib/admin/screens/profile_screen.dart
@@ -1,9 +1,12 @@
-import 'package:agape/auth/screens/password_reset_screen.dart';
+import 'package:agape/admin/screens/form_screen.dart';
+import 'package:agape/admin/screens/password_change.dart';
+import 'package:agape/auth/controllers/auth_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -11,7 +14,7 @@ class ProfileScreen extends StatelessWidget {
         elevation: 0,
         leading: IconButton(
           icon: const Icon(Icons.arrow_back,
-              color: const Color.fromARGB(255, 255, 255, 255)),
+              color: Color.fromARGB(255, 255, 255, 255)),
           onPressed: () {
             Navigator.pop(context);
           },
@@ -20,115 +23,131 @@ class ProfileScreen extends StatelessWidget {
             style: TextStyle(color: Color.fromARGB(255, 255, 255, 255))),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Stack(
-            clipBehavior: Clip.none,
+      body: FutureBuilder(
+        future: ref.read(authControllerProvider).getCurrentUser(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data == null) {
+            return const Center(child: Text('No user data available'));
+          }
+
+          final user = snapshot.data;
+          return Column(
             children: [
-              Container(
-                height: 200,
-                decoration: const BoxDecoration(
-                  color: Color.fromARGB(255, 9, 19, 58),
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(40),
-                    bottomRight: Radius.circular(40),
+              Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  Container(
+                    height: 200,
+                    decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 9, 19, 58),
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(40),
+                        bottomRight: Radius.circular(40),
+                      ),
+                    ),
                   ),
+                  Positioned(
+                    top: 120,
+                    left: MediaQuery.of(context).size.width / 2 - 60,
+                    child: CircleAvatar(
+                      radius: 60,
+                      backgroundImage: NetworkImage(
+                          user!["profile_image_url"] ??
+                              'https://via.placeholder.com/150'),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 80),
+              Text(
+                "${user['first_name']} ${user['middle_name']}" ??
+                    "Unknown User",
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
                 ),
               ),
-              Positioned(
-                top: 120,
-                left: MediaQuery.of(context).size.width / 2 - 60,
-                child: const CircleAvatar(
-                  radius: 60,
-                  backgroundImage:
-                      NetworkImage('https://via.placeholder.com/150'),
+              const SizedBox(height: 40),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    // Edit Profile button
+                    Expanded(
+                      child: SizedBox(
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color.fromARGB(
+                                255, 9, 19, 58), // Blue-black color
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        SubAdminForm(userId: user['id'])));
+                          },
+                          child: const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 6),
+                            child: Text(
+                              "EDIT PROFILE",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 20),
+                    Expanded(
+                      child: SizedBox(
+                        child: OutlinedButton(
+                          style: OutlinedButton.styleFrom(
+                            side: const BorderSide(
+                                color: Color.fromARGB(255, 9, 19, 58)),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      PasswordChangePage(userId: user["id"])),
+                            );
+                          },
+                          child: const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 6),
+                            child: Text(
+                              "CHANGE PASSWORD",
+                              style: TextStyle(
+                                color: Color(0xFF1A237E),
+                                fontSize: 12,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
-          ),
-          const SizedBox(height: 80),
-          const Text(
-            "Abebe Kebede",
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            ),
-          ),
-          const SizedBox(height: 40), 
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                // Edit Profile button
-                Expanded(
-                  child: SizedBox(
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromARGB(
-                            255, 9, 19, 58), // Blue-black color
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      onPressed: () {
-                        // Handle Edit Profile functionality
-                        // Navigator.push(
-                        //   context,
-                        //   MaterialPageRoute(
-                        //       builder: (context) => StudentEditProfilePage(user: user)),
-                        // );
-                      },
-                      child: const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 6),
-                        child: Text(
-                          "EDIT PROFILE",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 12,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 20),
-                Expanded(
-                  child: SizedBox(
-                    child: OutlinedButton(
-                      style: OutlinedButton.styleFrom(
-                        side: const BorderSide(
-                            color: Color.fromARGB(255, 9, 19, 58)),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ResetPasswordPage(email: '',)),
-                        );
-                      },
-                      child: const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 6),
-                        child: Text(
-                          "CHANGE PASSWORD",
-                          style: TextStyle(
-                            color: Color(0xFF1A237E),
-                            fontSize: 12,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
