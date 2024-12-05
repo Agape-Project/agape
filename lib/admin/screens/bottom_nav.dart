fca1a1e16f59e473ffed94be07bfc6b5fa8067fd
EphriamHab <ephremhabtamu2015@gmail.com>
2024-12-05 18:52:54 +0300

    bottom nav

--- lib/admin/screens/bottom_nav.dart
+++ lib/admin/screens/bottom_nav.dart
@@ -1,7 +1,7 @@
 import 'package:agape/admin/home.dart';
 import 'package:agape/admin/screens/setting_screen.dart';
 import 'package:agape/admin/screens/statics_screens.dart';
-import 'package:agape/common/record_list.dart';
+import 'package:agape/common/screens/record_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
