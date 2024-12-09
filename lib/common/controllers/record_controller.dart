1a2f412e043bae5aaae712c0870c89b5aa6edf30
EphriamHab <ephremhabtamu2015@gmail.com>
2024-12-09 20:13:36 +0300

    record controller

--- lib/common/controllers/record_controller.dart
+++ lib/common/controllers/record_controller.dart
@@ -2,13 +2,16 @@ import 'package:agape/common/repository/record_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 
-
-
-
 final disabilityRecordControllerProvider = Provider((ref) {
   final disabilityRecordRepository = ref.read(disabilityRecordRepositoryProvider);
   return DisabilityRecordController(disabilityRecordRepository);
 });
+
+final statsProvider = FutureProvider<Map<String, dynamic>>((ref) async {
+  final controller = ref.read(disabilityRecordControllerProvider);
+  return await controller.getStatistics();
+});
+
 
 class DisabilityRecordController {
   DisabilityRecordRepository disabilityRecordRepository;
@@ -53,15 +56,39 @@ class DisabilityRecordController {
     }
   }
 // filter records by query
-  Future<List<Map<String, dynamic>>> filterRecords(String query) async {
+Future<List<Map<String, dynamic>>> filterRecords({
+  String? gender,
+  String? region,
+  String? equipmentType,
+  String? month,
+  int? year,
+}) async {
+  try {
+    final response = await disabilityRecordRepository.filterRecords(
+      gender: gender,
+      region: region,
+      equipmentType: equipmentType,
+      month: month,
+      year: year,
+    );
+    return response;
+  } catch (e) {
+    // Handle and rethrow any error
+    rethrow;
+  }
+}
+
+
+  // search records
+  Future<List<Map<String, dynamic>>> searchRecords(String query) async {
     try {
-      final response = await disabilityRecordRepository.filterRecords(query);
+      final response = await disabilityRecordRepository.searchRecords(query);
       return response;
     } catch (e) {
       rethrow;
     }
   }
-  
+
   Future<String> deleteRecord(String id) async {
     try {
       final response = await disabilityRecordRepository.deleteRecord(id);
@@ -70,5 +97,14 @@ class DisabilityRecordController {
       rethrow;
     }
   }
-
+  
+  // get statistics
+  Future<Map<String, dynamic>> getStatistics() async {
+    try {
+      final response = await disabilityRecordRepository.getStats();
+      return response;
+    } catch (e) {
+      rethrow;
+    }
+  }
 }
